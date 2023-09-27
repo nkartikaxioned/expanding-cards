edd5a37a8a0d7f9efd63f97b9ce243e80d036ad8
nkartikaxioned <kartikn@axioned.com>
2023-09-27 16:58:33 +0530

    expanding-cards section js commit

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,6 +3,22 @@
 */
 
 
+const sections = document.querySelectorAll('.section');
+
+sections.forEach((section)=>{
+   
+    section.addEventListener('click',()=>{
+  
+        sections.forEach((section) => {
+            section.classList.remove('active')
+        })
+        section.classList.add('active')
+    })
+})
+
+
+
+
 
 
 
